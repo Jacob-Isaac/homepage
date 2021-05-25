acd6164eee9e3a5ec9bb3846c9436036b9f62bc4
Jakub Nowakowski <nowakowski.jakob@gmail.com>
2021-05-25 12:30:28 +0200

    functions simplified more

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,77 +1,76 @@
 {
-const imgMain = document.querySelector(".imgMain");
-const aboutMe = document.querySelector(".aboutMe");
-let condition = Boolean(true);
-let conditionFlag = Boolean(true);
+  const imgMain = document.querySelector(".imgMain");
+  const aboutMe = document.querySelector(".aboutMe");
+  let condition = Boolean(true);
+  let conditionFlag = Boolean(true);
 
-const sunImage = "images/sun.png";
-const moonImage = "images/moon.png";
+  const flagToggle = () => {
+    const language = document.querySelector(".js-brightAndLanguage__flag");
+    const polishFlag = "images/polish.png";
+    const englishFlag = "images/english.png";
 
+    if (conditionFlag === false) {
+      conditionFlag = true;
+      language.src = englishFlag;
+    } else if (conditionFlag === true) {
+      conditionFlag = false;
+      language.src = polishFlag;
+    }
+  };
+  const imgMainToggle = () => {
+    const ImageJacobFirst = "images/myfoto2.jpg";
+    const ImageError = "images/error.png";
 
-const moonSunButton = document.querySelector(".brightAndLanguage__moonSun");
-const langChange = document.querySelector(".brightAndLanguage__flag");
+    if (conditionFlag === false) {
+      imgMain.src = ImageError;
+    } else if (conditionFlag === true) {
+      imgMain.src = ImageJacobFirst;
+    }
+  };
+  const aboutMeTextToggle = () => {
+    if (aboutMe.innerHTML === "O mnie") {
+      aboutMe.innerHTML =
+        "<b>ERROR 666</b><br>We will provide English version as soon as possible! Sorry for the inconvenience.";
+    } else {
+      aboutMe.innerHTML = "O mnie";
+    }
+  };
+  const brightnessToggle = () => {
+    const body = document.querySelector(".body");
+    aboutMe.classList.toggle("aboutMe--colorWhite");
+    body.classList.toggle("body--dark");
+    imgMain.classList.toggle("imgMain--bright");
+  };
+  const moonSunIconToggle = () => {
+    const moonSunElement = document.querySelector(".js-brightAndLanguage__moonSun");
+    const sunImage = "images/sun.png";
+    const moonImage = "images/moon.png";
 
-const body = document.querySelector(".body");
-const moonSunElement = document.querySelector(".js-brightAndLanguage__moonSun");
-const eng = document.querySelector(".eng");
+    if (condition === false) {
+      condition = true;
+      moonSunElement.src = moonImage;
+    } else if (condition === true) {
+      condition = false;
+      moonSunElement.src = sunImage;
+    }
+  };
+  const init = () => {
+    const text =
+      "Wujek dobra rada: Jeśli późno wstaniesz, to możesz odrazu zjeść obiad i nie musisz robić śniadania";
+    console.log(text);
 
-const imgMainToggle = () => {
+    const langChange = document.querySelector(".brightAndLanguage__flag");
+    const moonSunButton = document.querySelector(".brightAndLanguage__moonSun");
 
-  const language = document.querySelector(".js-brightAndLanguage__flag");
-  const ImageJacobFirst = "images/myfoto2.jpg";
-  const ImageError = "images/error.png";
-  const polishFlag = "images/polish.png";
-  const englishFlag = "images/english.png";
-
-  if (conditionFlag===false) {
-    conditionFlag = true;
-    language.src = englishFlag;
-  } else if (conditionFlag===true){
-    conditionFlag = false;
-    language.src = polishFlag;
-  }
-
-  if (aboutMe.innerHTML === "O mnie") {
-    aboutMe.innerHTML =
-      "<b>ERROR 666</b><br>We will provide English version as soon as possible! Sorry for the inconvenience.";
-  } else {
-    aboutMe.innerHTML = "O mnie";
-  }
-
-  if (conditionFlag===false) {
-    imgMain.src = ImageError;
-  } else if (conditionFlag===true){
-    imgMain.src = ImageJacobFirst;
-  }
-}
-const brightnessToggle = () => {
-  body.classList.toggle("body--dark");
-  imgMain.classList.toggle("imgMain--bright");
-  aboutMe.classList.toggle("aboutMe--colorWhite");
-}
-const moonSunIconToggle = () => {
-  if (condition===false) {
-    condition = true;
-    moonSunElement.src = moonImage;
-  } else if (condition===true) {
-    condition = false;
-    moonSunElement.src = sunImage;
-  }
-}
-const init = () => {
-  const text =
-  "Wujek dobra rada: Jeśli późno wstaniesz, to możesz odrazu zjeść obiad i nie musisz robić śniadania";
-  console.log(text);
-
-  langChange.addEventListener("click", () => {
-  imgMainToggle();
-});
-  moonSunButton.addEventListener("click", () => {
-  brightnessToggle();
-  moonSunIconToggle();
-});
-  
-
-}
- init();
+    langChange.addEventListener("click", () => {
+      flagToggle();
+      imgMainToggle();
+      aboutMeTextToggle();
+    });
+    moonSunButton.addEventListener("click", () => {
+      brightnessToggle();
+      moonSunIconToggle();
+    });
+  };
+  init();
 }
